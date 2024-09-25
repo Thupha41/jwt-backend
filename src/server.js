8b55b1fdb8790a023b58f2fa494387d462441a22
Thupha41 <phatngo.040103@gmail.com>
2024-09-25 20:23:44 +0700

    Set up environment

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+import configViewEngine from "./configs/viewEngine";
+import webRoutes from "./routes/web";
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+
+//config template engine
+configViewEngine(app);
+
+//config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Khai bao route
+app.use("/", webRoutes);
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`);
+});
